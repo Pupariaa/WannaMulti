cd4705f683a0f0bf141eb8d87201cea6cbe9a16d
Maxime <32194766+Pupariaa@users.noreply.github.com>
2024-05-02 01:22:04 +0200

    create a first IRC command, help

---
+++ src/commands/irc/help.js
@@ -0,0 +1,4 @@
+async function Static(){
+    return 'Créer un multi en utilisant la commande !host nom date heure mode tag | Format: !host "nom du multi" 02-05-2024 18:00 STD "Jump !" |  Modes: STD, MANIA, CTB, TAIKO | Assure-toi d\'avoir rejoint le [discord](https://discord.puparia.fr/wannamulti) pour effectuer cette commande.'
+}
+module.exports = {Static};
